5f6beef2a6ea97578aa9733de1be65e9d28d0f73
dedalovsicaro <dedalovsicaro@gmail.com>
2020-03-28 18:59:22 +0000

    added files for html client

--- src/api.py
+++ src/api.py
@@ -1,4 +1,7 @@
 import flask
+from flask import render_template
+
+name = 'hola'
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,4 +11,15 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Welcome to the robot arm project</h1><p>This site is a prototype API for distant manage of a robot arm.</p>"
 
+@app.route('/panel')
+def panel():
+    return render_template('panel.html',  name=name)
+
+@app.route('/servo/<servoname>/<int:anlge>')
+def servo(servoname, angle):
+    return 'Hello, World'
+
 app.run()
+
+
+
